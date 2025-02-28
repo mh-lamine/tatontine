c9f40052dcca6cc9db3bb9707bad579345c73ee7
mh-lamine <mlamins.ngom@gmail.com>
2025-02-28 10:41:21 +0100

    init

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center bg-blue-500">
+      <Text className="text-white text-lg">Hello, NativeWind!</Text>
+    </View>
+  );
+}
